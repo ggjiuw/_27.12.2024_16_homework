52646f2993c33e97ac7d315f63c9b479fab0664f
ggjiuw <aeakgm@gmail.com>
2024-12-28 14:14:31 +0200

    add one more test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,5 +7,9 @@ public class MainTest {
         assertEquals(3, Main.StringCounter("hello,world,java"));
         assertEquals(0, Main.StringCounter(null));
         assertEquals(2, Main.StringCounter("1 hat3 jun1t, ye2"));
+        assertEquals(1, Main.StringCounter("s2tr2in2g"));
+
+        assertEquals(4, Main.StringCounter("I FIXED THIS CODE, y2a2y"));
+
     }
 }
