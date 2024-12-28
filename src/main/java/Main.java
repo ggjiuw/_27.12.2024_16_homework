8b4acf011925e51155c55220946d85ce7c2ae00c
ggjiuw <aeakgm@gmail.com>
2024-12-28 14:14:01 +0200

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,36 @@ public class Main {
 
         int words_counter = 0;
         Character[] replace = {',', '.', '[', ']', '{', '}', '/', '-', ' '};
+        Character[] digits = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
 
-        for (Character character : replace)
+        for (Character character : replace) {
             string = string.replace(character, ';');
-
-
-        String[] splited_string = string.split(";");
-
-        Character[] replace_digits = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-
-
-        for (String element : splited_string) {
-            for (Character digit : replace_digits)
-                element = element.replace(digit, ' ');
-
-            if (element.length() >= 3) {
-                if (!(element.split(" ").length >= 2))
-                    words_counter++;
-            }
         }
+
+        for (Character digit : digits) {
+            string = string.replace(digit, '\\');
+        }
+
+        String[] split = string.split("");
+
+        int word_length = 0;
+        for (String character : split) {
+            if (character.equals(";")) {
+                if (word_length >= 3)
+                    words_counter++;
+
+                word_length = 0;
+                continue;
+            }
+
+            if (character.equals("\\")) {
+                continue;
+            }
+
+            word_length++;
+        }
+        if (word_length >= 3)
+            words_counter++;
 
         return words_counter;
     }
